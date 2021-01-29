b6e01643fdf60e6a1a99ea972364aebbb840d719
David Cabrera <davidcabrera052196@gmail.com>
2021-01-28 20:32:47 -0500

    Deleted var and val meaningless variables

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -8,9 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       var a = 3
-       val b = 43
-
 
     }
 }
